9b4a5c30b09f0a87664022eda3c9e70a79a4723a
Jack Beoris <john.beoris@utexas.edu>
2019-09-25 21:38:27 -0500

    fix typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,6 +3,9 @@ declare module "*.json" {
   export default value;
 }
 
+declare module 'text-to-mp3';
+declare module 'uuid/v4';
+
 interface ChannelMessage {
   channelId: string
   message: string
